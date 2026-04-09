d180f8fd1ae6f187d9688daba287006de4123b4e
Muhil <muhilvannan.elavazhagan@gmail.com>
2026-04-09 10:24:44 -0400

    Add delete_task function to remove tasks
    Adding function delete_tasks() to delete certain tasks.

--- tasks.py
+++ tasks.py
@@ -40,3 +40,17 @@ def save_tasks(tasks):
     with open("data.txt", "w") as file:
         for t in tasks:
             file.write(f"{t['task']}|{t['done']}\n")
+
+def delete_task(tasks):
+    view_tasks(tasks)
+    if not tasks:
+        return
+    try:
+        index = int(input("Enter task number to delete: ")) - 1
+        if 0 <= index < len(tasks):
+            tasks.pop(index)
+            print("Task successfully deleted !")
+        else:
+            print("Invalid number.")
+    except ValueError:
+        print("Enter a valid number.")
